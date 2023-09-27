99051a4d4b3474e22c2e000c42399e2bbad38bb2
agueriv <rivasguerreroariel@gmail.com>
2023-09-27 20:40:48 +0200

    Primeros pasos de implementación de clase Jugador

--- js/modules/Jugador.js
+++ js/modules/Jugador.js
@@ -1,1 +1,13 @@
-class Jugador {}
+class Jugador {
+  constructor(nombre) {
+    this.nombre = nombre;
+    this.vidas = 9;
+  }
+
+  /**
+   * Resta una vida al total de vidas del jugador
+   */
+  restarVida() {
+    this.vida -= 1;
+  }
+}
