cc8133449f28e5983497858ae5df5d6e0621d11e
AdrianBernalMolina <abermol1605@ieszaidinvergeles.org>
2023-09-28 19:22:55 +0200

    palabra

--- Ahorcado/Index.js
+++ Ahorcado/Index.js
@@ -1,5 +1,8 @@
-size = 5;
+let size = 5;
+let ini = "";
 for (var i = 0; i < size; i++) {
     ini = ini + "_ ";
 }
-document.getElementById("word").outerText = ini;
+
+document.getElementById("word").innerHTML = "hola";//ini
+document.getElementById("check").onclick()
