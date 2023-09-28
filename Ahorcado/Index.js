2d121ad5f4df375701d28aadd59fa2a8c767db5a
AdrianBernalMolina <abermol1605@ieszaidinvergeles.org>
2023-09-28 19:48:56 +0200

    hacer la funcion cuando clicke check

--- Ahorcado/Index.js
+++ Ahorcado/Index.js
@@ -1,8 +1,10 @@
+//import {} from 
 let size = 5;
 let ini = "";
 for (var i = 0; i < size; i++) {
     ini = ini + "_ ";
 }
 
-document.getElementById("word").innerHTML = "hola";//ini
-document.getElementById("check").onclick()
+document.getElementById("check").onclick = function palabra() {
+    document.getElementById("word").innerHTML = ini
+}
