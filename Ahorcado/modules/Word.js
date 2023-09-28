9bc47c330c0455558c82a22642dd9e35727a7835
AdrianBernalMolina <abermol1605@ieszaidinvergeles.org>
2023-09-28 20:27:38 +0200

    metodo para devolver la palabra

--- Ahorcado/modules/Word.js
+++ Ahorcado/modules/Word.js
@@ -1,8 +1,10 @@
 class Word {
     constructor(){
-        random = Math.floor(Math.random() * 11);
-        const word = ["zapato", "puerta", "habitacion", "edificio", "pantalla"];
+        random = Math.floor(Math.random() * 6);
+        const word = ["zapato", "puerta", "habitacion", "edificio", "pantalla"]
     }
-
-    //document.getElementById("textoCambiante").innerHTML = texto1[n%texto1.length];
+    returnWord() {
+        return word[random];
+    }
 }
+export { Word }
