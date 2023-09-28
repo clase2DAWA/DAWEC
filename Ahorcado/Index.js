6f551fca5a608b0dfe2b03b1ddd09653ab9fcab5
AdrianBernalMolina <abermol1605@ieszaidinvergeles.org>
2023-09-28 20:27:03 +0200

    import

--- Ahorcado/Index.js
+++ Ahorcado/Index.js
@@ -1,9 +1,12 @@
-//import {} from 
+import { Word } from "Word.js";
+
 let size = 5;
 let ini = "";
 for (var i = 0; i < size; i++) {
     ini = ini + "_ ";
 }
+
+const palabra = new Word();
 
 document.getElementById("check").onclick = function palabra() {
     document.getElementById("word").innerHTML = ini
