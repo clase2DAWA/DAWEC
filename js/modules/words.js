e69568a43f93f18ee0e3af8592d413fc4a6e1217
SantiagoVilchesRomero <svilches1212@ieszaidinvergeles.org>
2023-09-28 20:47:04 +0200

    implementamos array y sacamos palabra aleatoria

--- js/modules/words.js
+++ js/modules/words.js
@@ -1,3 +1,23 @@
-class words{
-    
+class words {
+    constructor() {
+        //Arrays de palabras
+        this.palabras = [
+            { palabra: "ordenador" },
+            { palabra: "ratón" },
+            { palabra: "teclado" },
+            { palabra: "pantalla" },
+            { palabra: "gráfica" },
+            { palabra: "tarjeta" },
+            { palabra: "cámara" },
+            { palabra: "cables" },
+            { palabra: "placa base" },
+            { palabra: "ventiladores" },
+        ];
+    }
+
+    obtenerPalabraAleatoria() {
+        const indiceAleatorio = Math.floor(Math.random() * this.palabras.length);
+        return this.palabras[indiceAleatorio];
+    }
+
 }
