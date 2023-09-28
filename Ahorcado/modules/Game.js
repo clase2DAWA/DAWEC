cab2cdf09d6300dadb3ad691195b805d13fb517a
AdrianBernalMolina <abermol1605@ieszaidinvergeles.org>
2023-09-28 19:13:55 +0200

    cambio de lado

--- Ahorcado/modules/Game.js
+++ Ahorcado/modules/Game.js
@@ -1,6 +1,13 @@
 class Game {
     constructor() {
+        this.size = word.length
         this.palabra = palabra
+        this.ini = ini
     }
-    
+    wordSize() {
+        for (var i = 0; i < size; i++) {
+            ini = ini + "_ ";
+        }
+        
+    }
 }
---
+++ Ahorcado/modules/Word.js
@@ -0,0 +1,8 @@
+class Word {
+    constructor(){
+        random = Math.floor(Math.random() * 11);
+        const word = ["zapato", "puerta", "habitacion", "edificio", "pantalla"];
+    }
+
+    //document.getElementById("textoCambiante").innerHTML = texto1[n%texto1.length];
+}
