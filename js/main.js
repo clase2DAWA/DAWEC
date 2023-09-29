0558bbcc27e07166ac9fd7a26f22ca138e98c1c3
agueriv <rivasguerreroariel@gmail.com>
2023-09-29 18:27:31 +0200

    Implementación de jugabilidad + display

--- js/main.js
+++ js/main.js
@@ -6,11 +6,17 @@ let disClue = document.getElementById("clue");
 let disLive = document.getElementById("lives");
 let letterIn = document.getElementById("userCaracter");
 let checkBtn = document.getElementById("check");
+let alimentacion = document.getElementById("retroAlimentacion");
+let askNewGame = document.getElementById("newGame");
 
+// Juego
 let partida = new GAME.game("Jugador1");
 
+// Ocultar palabra
+partida.hidesWord();
+
 // Mostramos la palabra oculta
-disWord.innerHTML = partida.hidesWord();
+disWord.innerHTML = partida.showHideWord();
 
 // Seteamos la pista de la palabra
 disClue.innerHTML = partida.word.pista;
@@ -22,5 +28,22 @@ disLive.innerHTML += partida.player.vidas;
 
 checkBtn.onclick = function () {
   // Comprobar el intento y Actualizar salidas
-  disWord.innerHTML = partida.checkTry(letterIn.value);
+  if (partida.checkTry(letterIn.value)) {
+    disWord.innerHTML = partida.showHideWord();
+    if (partida.acerted()) {
+      alimentacion.innerHTML = "HAS GANAO PRIMO";
+      askForLeave();
+    }
+  } else {
+    alimentacion.innerHTML = partida.ERROR;
+    disLive.innerHTML = "Tus vidas: " + partida.player.restarVida();
+    if (partida.player.vidas < 1) {
+      alimentacion.innerHTML = "HAS PERDIDO PISHA";
+      askForLeave();
+    }
+  }
 };
+
+function askForLeave() {
+  askNewGame.style.display = "block";
+}
