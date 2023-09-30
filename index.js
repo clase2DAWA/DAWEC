2c6cfdcbbe071387495139a76674524b77e52f73
VitoFernandez <fernandezherediavictorino@gmail.com>
2023-09-30 22:33:14 +0200

    fail letter

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-import {Game} from "./modules/game.js";
+import { Game } from "./modules/game.js";
 
 let game = new Game();
 document.getElementById("lives").innerHTML = game.player.lives;
 game.undercores();
 document.getElementById("word").innerHTML = game.arrUndercores;
-console.log(game.word);
+game.ocurrences(document.getElementById("userLetter").value);
 
-document.getElementById("send").onclick = function(){document.getElementById("word").innerHTML = game.arrWord};
-document.getElementById("send").onclick = function(){document.getElementById("word").innerHTML = game.arrWord};
+document.getElementById("send").onclick = function () {
+    game.remplace(document.getElementById("userLetter").value);
+    document.getElementById("word").innerHTML = game.arrUndercores;
+    if ((document.getElementById("userLetter").value).length < 2) {
+        game.useLetters.push((document.getElementById("userLetter").value));
+        document.getElementById("mensage").innerHTML = "";
+    } else {
+        document.getElementById("mensage").innerHTML = "Introduce only one character.";
+    }
+   
+
+};
+
+
+let a = [1,2,3];
+let b = [1,2,3];
+
+
+if(a === b){
+    console.log("hola");
+}
+
+
+
+
 
 
 
--- modules/game.js
+++ modules/game.js
@@ -6,24 +6,32 @@ class Game {
     constructor() {
         this.name = "vito";
         this.word = new Word().word();
-        this.player = new Player(name);
-        this.arrWord = this.word.split('');
-        this.secretWord = this.word
+        this.player = new Player(this.name);
+        this.arrCharactWord = this.word.split('');
         this.arrUndercores = [];
-        this.arrLetters = [];
+        this.arrOcurrences = [];
+        this.useLetters = [];
+
     };
-    undercores(){
+    undercores() {
         for (let i = 0; i < this.word.length; i++) {
             this.arrUndercores.push("_");
-        } 
+        }
     };
     ocurrences(letter) {
         let position = this.word.indexOf(letter);
         while (position != -1) {
-            this.arrLetters.push(position);
+            this.arrOcurrences.push(position);
             position = this.word.indexOf(letter, position + 1);
         }
     };
+    remplace(letter) {
+        let index = this.arrCharactWord.indexOf[letter];
+        while (index != -1) {
+            this.arrUndercores[index] = letter;
+            index = this.arrCharactWord.indexOf(letter, index + 1);
+        }
+    }
 
 };
 export { Game };
