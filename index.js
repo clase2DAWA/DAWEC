8e5f20b1b438804b7184553bd042bb015d1c8207
VitoFernandez <fernandezherediavictorino@gmail.com>
2023-09-30 16:02:13 +0200

    ahorcado nuevo

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import {Game} from "./modules/game.js"
 
 let game = new Game();
-
 document.getElementById("lives").innerHTML = game.player.lives;
+document.getElementById("word").innerHTML = game.arrUnderscores;
+let letterUser = document.getElementById("submit").value;
+document.getElementById("submit").addEventListener("click", secretWord());
+document.getElementById("word").innerHTML = game.underscores();
+function secretWord(){
+    document.getElementById("word").innerHTML = game.underscores();
+}
--- modules/game.js
+++ modules/game.js
@@ -1,38 +1,38 @@
-import { Word } from "./modules/word.js";
-import { Player } from "./modules/player.js";
+import { Word } from "./word.js";
+import { Player } from "./player.js";
 
 class Game {
 
     constructor() {
 
-        this.playerName = prompt("Enter your user name.");
+        // this.playerName = prompt("Enter your user name.");
         this.player = new Player(this.playerName);
         this.word = new Word().word();
         this.arrLetter = this.word.split('');
-        this.arrUnderscores = underscores();
-        this.arrOcurrences = this.ocurrences();
     }
 
     showLetter(letter) {
         for (let i = 0; i < this.arrLetter.length; i++) {
-            letterUser = letter;
-            indice = arrOcurrences[i];
-            underscores[indice] = letterUser;
+            let letterUser = letter;
+            let indice = this.arrOcurrences[i];
+            this.arrUnderscores[indice] = letterUser;
         }
     }
+    underscores(){  
+        let underscores = [];  
+            for (let i = 0; i < this.word.length; i++) {
+                underscores.push("_");
+            }  
+            return underscores;
+    }
     ocurrences(letter) {
-        letters = [];
-        position = word.indexOf(letter);
+        let letters = [];
+        let position = this.word.indexOf(letter);
         while (position != -1) {
             letters.push(position);
             position = word.indexOf(letter, position + 1);
         }
         return letters;
     }
-    underscores() {
-        for (let i = 0; i < this.word.length; i++) {
-            this.arrUnderscores.push("_");
-        }
-    }
 };
-export {Game};
+export { Game };
