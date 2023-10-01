bf1749d8725fe3be3f69ce108654d94aef8d2a89
fransanchezp <fjsp02@gmail.com>
2023-10-01 02:04:33 +0200

    Lo primero que he hecho ha sido instanciar Word y Player. Tras esto en la clase Game me he encargado de que la clase se encargue de que un jugador tenga 10 vidas para adivinar una palabra letra por letra y que tanto si pierde o gane se le diga y se acabe el juego. Solo falta que mejore la parte grafica

--- ahorcado/game.js
+++ ahorcado/game.js
@@ -1,12 +1,40 @@
 import { Word } from "./word.js";
 import { Player } from "./player.js";
 
-let word = Word.randomWord;
-let letter2 = Player.letter;
+let word = new Word().randomWord;
+let letter2 = new Player().letter;
 
 
 console.log(word);
 console.log(letter2);
+
 export let Game = class Game {
-    
+    constructor() {
+        this.word = new Word().randomWord();
+        this.player = new Player();
+        this.lives = 10;
+        this.gameOver = false;
+    }
+
+    correctLetter() {
+        const theletter = this.player.letter;
+
+        if(this.gameOver){
+            return "GAME OVER";
+        }
+
+        const thisLetter = this.player.letter;
+
+        if (!this.word.includes(thisLetter)) {
+            this.lives--;
+        } else if (this.lives <= 0) {
+            this.gameOver = true;
+            return "You lost..." + " the word was: " + this.word;
+        } else if(!this.word.includes("_")) {
+            this.gameOver = true;
+            return "¡CONGRATULATIONS, YOU WIN!";
+        }
+        return "Continues to play" + this.word;
+    }
+
 }
--- ahorcado/word.js
+++ ahorcado/word.js
@@ -4,7 +4,7 @@ export let Word = class Word {
     randomWord() {//metodo de la clase word
         const secretWord = Math.floor(Math.random() * words.length);
         let showWord = "";//para ahora en el for id agregando _ segun la long de la palabra
-        for (let index = 0; index < secretWord.length; index++) {
+        for (let index = 0; index < words[secretWord].length; index++) {
             showWord += "_";
         }
         console.log(secretWord, words[secretWord]);
