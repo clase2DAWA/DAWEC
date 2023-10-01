81a605f84d7962dd5d1330293194967e1aa624b7
aurelioVSM <avsm.grx@gmail.com>
2023-10-01 16:18:54 +0200

    Juego Terminado

--- game.js
+++ game.js
@@ -5,6 +5,48 @@ class Game {
         this.wordInstance = new Word();
         this.word = this.wordInstance.randomWord().toUpperCase().split('');
         this.elementWord = document.getElementById("hiddenWord");
+        this.secondsRemaining = 60;
+    }
+
+
+    stop() {
+        this.secondsRemaining = -1;
+    }
+
+    winner() {
+        console.log("GANASTE")
+        this.stop();
+        countdown.innerHTML = "YOU WIN";
+        countdown.style.color = 'green';
+    }
+
+    // LLeva a cabo la generacion de una cuenta atras 
+    countdown() {
+
+        const interval = setInterval(() => {
+            if (this.secondsRemaining === -1) {
+                clearInterval(interval); // Detiene la cuenta regresiva cuando llegue a 0
+                let countdown = document.getElementById("countdown");
+                // countdown.innerHTML = "GAME OVER";
+
+                var buttons = document.getElementsByClassName("button");
+
+                for (var i = 0; i < buttons.length; i++) {
+                    buttons[i].disabled = true;
+                }
+
+            } else {
+                let cont = document.getElementById("countdown");
+                cont.textContent = this.secondsRemaining;
+                if (this.secondsRemaining <= 30) {
+                    cont.style.color = "orange";
+                    if (this.secondsRemaining <= 10) {
+                        cont.style.color = "crimson";
+                    }
+                }
+                this.secondsRemaining--;
+            }
+        }, 1000); // 1000 milliseconds = 1 second
     }
 
     play() {
@@ -12,6 +54,12 @@ class Game {
         for (let index = 0; index < this.word.length; index++) {
             this.elementWord.textContent += "_";
         }
+        this.countdown();
+
+        document.getElementById('restart').onclick = () => {
+            window.location.reload();
+        };
+
     }
 
     load(letter) {
@@ -29,6 +77,10 @@ class Game {
             wordArray[index] = letter;
         }
         this.elementWord.textContent = wordArray.join('');
+
+        if (!this.elementWord.textContent.includes("_")) {
+            this.winner();
+        }
     }
 }
 const wordInstance = new Word();
--- player.js
+++ player.js
@@ -1,6 +1,7 @@
 class Player {
     constructor(game) {
         this.game = game; // Almacenamos la instancia de Game en una propiedad
+        this.life = 10;
     }
 
     pressedLetter() {
@@ -15,8 +16,39 @@ class Player {
                 const letter = event.target.value;
                 console.log('Botón pulsado:', letter);
                 this.game.load(letter); // Llama al método load de la instancia de Game
+
+                // Comprueba si la letra es correcta y cambia su color
+                if (this.game.word.includes(letter)) {
+                    event.target.style.color = 'green';
+                    event.target.style.fontWeight = 'bold';
+                } else {
+                    event.target.style.color = 'crimson';
+                    event.target.style.fontWeight = 'bold';
+                    this.life--;
+                    if (this.life >= 0) {
+                        let countLife = document.getElementById("lifeCounter");
+                        countLife.innerHTML = this.life;
+                        this.replaceIMG(this.life);
+
+                        if (this.life == 0) {
+                            this.game.stop();
+                            let countdown = document.getElementById("countdown");
+                            countdown.innerHTML = "GAME OVER";
+                            countdown.style.color = 'crimson';
+                        }
+                    }
+                }
+
+                // Desactiva el botón después de hacer clic en él
+                button.disabled = true;
             });
         });
+    }
+
+    //Reemplaza la imagen segun las vidas que te queden
+    replaceIMG(life) {
+        let imagen = document.getElementById("graphics");
+        imagen.src = "TimeLapse/" + life + ".png";
     }
 }
 
